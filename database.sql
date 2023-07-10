0a741d0643e0a07d14ef9ea247561a19b4ed5468
longyan <longyan@nit.zju.edu.cn>
2023-07-10 16:32:42 +0800

    upload sql file to generate database, table and data

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS test;
+USE test;
+
+CREATE TABLE IF NOT EXISTS employee(
+    id int auto_increment primary key,
+    name varchar(100),
+    salary double(10, 2),
+    job varchar(100)
+);
+
+INSERT INTO employee(name, salary, job) VALUES ('佟湘玉', 8000.00, '掌柜');
+INSERT INTO employee(name, salary, job) VALUES ('白展堂', 7000.00, '跑堂');
+INSERT INTO employee(name, salary, job) VALUES ('李大嘴', 1000.00, '初始');
+INSERT INTO employee(name, salary, job) VALUES ('吕秀才', 10000.00, '会计');
+INSERT INTO employee(name, salary, job) VALUES ('郭芙蓉', 9000.00, '打杂');
