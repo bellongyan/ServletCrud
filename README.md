8e918a3f45902f2a569d52a250d7b28799c5ffcf
longyan <longyan@nit.zju.edu.cn>
2023-07-11 22:03:50 +0800

    update README.md, database.sql and the view of the login and register

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # ServletCrud
 JSP中Servlet的crud操作
 
+## 工具及版本
+
+|工具|版本|
+|---|---|
+|IntelliJ IDEA|2023.1.3(Ultimate Edition)|
+|Project SDK|Amazon Corretto version 1.8.0_372|
+|Apache Tomcat|9.0.75|
+|Database|8.0.33 MySQL Community Server|
+|JDBC driver|mysql-connector-j-8.0.32|
+
+## 页面
+
+注册页面
+
+![注册页面](./img/register.png)
+
+登录页面
+
+![登录页面](./img/login.png)
+
 主页面
 
 ![主页面](./img/list.png)
--- database.sql
+++ database.sql
@@ -13,3 +13,12 @@ INSERT INTO employee(name, salary, job) VALUES ('白展堂', 7000.00, '跑堂');
 INSERT INTO employee(name, salary, job) VALUES ('李大嘴', 1000.00, '初始');
 INSERT INTO employee(name, salary, job) VALUES ('吕秀才', 10000.00, '会计');
 INSERT INTO employee(name, salary, job) VALUES ('郭芙蓉', 9000.00, '打杂');
+
+CREATE TABLE IF NOT EXISTS admin(
+    username varchar(100) primary key not null,
+    name varchar(100) not null,
+    password varchar(20) not null,
+    int sex,
+);
+
+INSERT INTO admin(username, name, password, sex) VALUES ('test', 'testname', '111111', 1);
